5cc96da28de61d64fdf391c7e26d5a21a5646940
ltev <ltev@interia.pl>
2024-11-26 18:55:20 +0100

    Added RecipeRepository

---
+++ src/main/java/com/ltev/spring6recipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,11 @@
+package com.ltev.spring6recipeapp.repositories;
+
+import com.ltev.spring6recipeapp.domains.Ingredient;
+import com.ltev.spring6recipeapp.domains.Recipe;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+}

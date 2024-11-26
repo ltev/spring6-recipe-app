36b74afbac340c2c6034d53e9334a2c0f6d6b100
ltev <ltev@interia.pl>
2024-11-26 18:56:18 +0100

    Updated Recipe and Note to JPA Entities and setting their relationship to bidirectional OneToOne

--- src/main/java/com/ltev/spring6recipeapp/bootstrap/StartUpDataLoader.java
+++ src/main/java/com/ltev/spring6recipeapp/bootstrap/StartUpDataLoader.java
@@ -1,29 +1,27 @@
 package com.ltev.spring6recipeapp.bootstrap;
 
-import com.ltev.spring6recipeapp.domains.Ingredient;
-import com.ltev.spring6recipeapp.domains.UnitOfMeasure;
+import com.ltev.spring6recipeapp.domains.*;
 import com.ltev.spring6recipeapp.repositories.IngredientRepository;
+import com.ltev.spring6recipeapp.repositories.RecipeRepository;
 import com.ltev.spring6recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
 
 @Configuration
+@AllArgsConstructor
 public class StartUpDataLoader implements CommandLineRunner {
 
+    RecipeRepository recipeRepository;
     UnitOfMeasureRepository unitOfMeasureRepository;
     IngredientRepository ingredientRepository;
-
-    public StartUpDataLoader(UnitOfMeasureRepository unitOfMeasureRepository, IngredientRepository ingredientRepository) {
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
-        this.ingredientRepository = ingredientRepository;
-    }
 
     @Override
     public void run(String... args) throws Exception {
         UnitOfMeasure gram = new UnitOfMeasure("gram");
-        unitOfMeasureRepository.save(gram);
+        // unitOfMeasureRepository.save(gram);      // detached entity to persist exception in ingredientRepository save
 
         UnitOfMeasure teaspoon = new UnitOfMeasure("teaspoon");
 
@@ -31,6 +29,7 @@ public class StartUpDataLoader implements CommandLineRunner {
         ingredient.setDescription("Olive Oil");
         ingredient.setAmount(new BigDecimal("2.5"));
         ingredient.setUom(gram);
+
         try {
             ingredientRepository.save(ingredient);
         } catch (Exception e) {
@@ -42,5 +41,18 @@ public class StartUpDataLoader implements CommandLineRunner {
 
         ingredient.setUom(new UnitOfMeasure("liter"));
         ingredientRepository.save(ingredient);
+
+        Recipe recipe = new Recipe();
+        recipe.setDescription("New recipe with a lot of olive oil");
+        recipe.setPrepTime(45);
+        recipe.setServings(4);
+        recipe.setDifficulty(Difficulty.HARD);
+        recipe.setNote(new Note("This is a note for the new recipe with a lot of olive oil"));
+        recipeRepository.save(recipe);
+
+
+        Recipe foundRecipe = recipeRepository.findById(1L).get();
+        System.out.println(foundRecipe);
+
     }
 }
--- src/main/java/com/ltev/spring6recipeapp/domains/Note.java
+++ src/main/java/com/ltev/spring6recipeapp/domains/Note.java
@@ -1,8 +1,30 @@
 package com.ltev.spring6recipeapp.domains;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
 public class Note {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Recipe recipe;
     private String recipeNote;
+
+    @OneToOne(mappedBy = "note")    // Recipe -> note
+    private Recipe recipe;
+
+    public Note(String recipeNote) {
+        this.recipeNote = recipeNote;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", recipeNote='" + recipeNote + '\'' +
+                ", recipeId=" + (recipe != null ? recipe.getId() : null) +
+                '}';
+    }
 }
